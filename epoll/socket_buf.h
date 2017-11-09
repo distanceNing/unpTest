28b245e6130e89fdbd65201ddf31064faec0cad8
yangning <distancening@gmail.com>
2017-11-09 17:58:57 +0800

    code style

--- epoll/socket_buf.h
+++ epoll/socket_buf.h
@@ -20,18 +20,18 @@ class SocketBuf
 public:
 	static const size_t kInitSize = 1024;
 	static const size_t kPrepend = 8;
-    SocketBuf(size_t init_size = kInitSize, size_t prepend = kPrepend)
-            :readIndex_(prepend), writeIndex_(prepend), capacity_(prepend+init_size)
-    {
+	SocketBuf(size_t init_size = kInitSize, size_t prepend = kPrepend)
+		:readIndex_(prepend), writeIndex_(prepend), capacity_(prepend + init_size)
+	{
 		buffer_ = new char[capacity_];
-    }
+	}
 	//write
 	void append(const std::string str)
 	{
 		write(str.c_str(), str.size());
 	}
 
-    void write(const char* txt, size_t len);
+	void write(const char* txt, size_t len);
 
 	bool write(net::SocketBuf& buf, size_t bytes);
 
@@ -41,7 +41,7 @@ public:
 		readIndex_ += len;
 	}
 
-    bool read(char* txt, size_t bytes);
+	bool read(char* txt, size_t bytes);
 
 	bool read(net::SocketBuf& buf, size_t bytes);
 
@@ -59,28 +59,28 @@ public:
 		return ret;
 	}
 
-    ~SocketBuf()
-    {
+	~SocketBuf()
+	{
 		delete[] buffer_;
 		buffer_ = nullptr;
 		capacity_ = 0;
-    }
+	}
 private:
 	const int kGrowthFactor = 1.5;
 
 	void grow(size_t len);
-	
+
 	const char* begin() const
 	{
 		return buffer_;
 	}
 
-	char* begin() 
+	char* begin()
 	{
 		return buffer_;
 	}
 
-	char* readBegin() 
+	char* readBegin()
 	{
 		return begin() + readIndex_;
 	}
@@ -118,8 +118,8 @@ private:
 
 	char*  buffer_;
 	size_t capacity_;
-    size_t readIndex_;
-    size_t writeIndex_;
+	size_t readIndex_;
+	size_t writeIndex_;
 
 };
 
